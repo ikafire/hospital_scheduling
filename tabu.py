7d4682e271271b42d4f50992387781073e2bdaa6
Ching Lin <clin@bridgewell.com>
2022-12-06 01:01:58 +0800

    feat: extract tabu and add test

---
+++ tabu.py
@@ -0,0 +1,49 @@
+def better_than(sol1, sol2):
+    if not sol2:
+        return sol1
+    else:
+        return sol1.fitness() > sol2.fitness()
+
+class Tabu:
+    def __init__(self):
+        self.max_iter = 10
+
+    def solve(self, solution):
+        best = solution
+        current = solution
+        tabu_list = []
+        tabu_list.append(best)
+
+        i = 0
+        while i < self.max_iter:
+            neighbors = current.get_neighbors()
+            if not neighbors:
+                break
+
+            best_non_tabu_neighbor = None
+            best_neighbor = None
+            for neighbor in neighbors:
+                if better_than(neighbor, best_neighbor):
+                    best_neighbor = neighbor
+                if (not neighbor in tabu_list) and better_than(neighbor, best_non_tabu_neighbor):
+                    best_non_tabu_neighbor = neighbor
+
+            # ignore tabu rule if nowhere to go
+            if best_non_tabu_neighbor:
+                current = best_non_tabu_neighbor
+            else:
+                current = best_neighbor
+
+            if current.fitness() > best.fitness():
+                best = current
+
+            if current not in tabu_list:
+                tabu_list.append(current)
+
+            print (best)
+            print (current)
+            print (tabu_list)
+
+            i = i + 1
+
+        return best
---
+++ test_tabu.py
@@ -0,0 +1,48 @@
+from tabu import Tabu
+
+def test_no_neighbor():
+    tabu = Tabu()
+    initial_state = FakeSolution(1)
+
+    result = tabu.solve(initial_state)
+
+    assert result == initial_state
+
+def test_simple_case():
+    tabu = Tabu()
+    initial_state = FakeSolution(1)
+    initial_state.add_neighbor(FakeSolution(3))
+    temp = FakeSolution(2)
+    temp.add_neighbor(FakeSolution(4))
+    initial_state.add_neighbor(temp)
+
+    result = tabu.solve(initial_state)
+
+    assert result == FakeSolution(4)
+
+class FakeSolution:
+    def __init__(self, score):
+        self.score = score
+        self.neighbors = []
+
+    def fitness(self):
+        return self.score
+
+    def add_neighbor(self, other):
+        self.neighbors.append(other)
+        other.neighbors.append(self)
+
+    def get_neighbors(self):
+        return self.neighbors
+
+    def __eq__(self, other):
+        return self.score == other.score
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return str(f'score {self.score}')
+
+    def __repr__(self):
+        return self.__str__()
